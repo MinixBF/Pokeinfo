dca85631e6c26f0bd52240da71ef5cfd414f8d42
MinixBF <benj.ferre@hotmail.fr>
2020-05-04 11:01:28 +0200

    [Feature][API] Initialisation de l'api

---
+++ app/src/main/java/com/exemple/td3_recyclerview/RestPokemonResponse.java
@@ -0,0 +1,21 @@
+package com.exemple.td3_recyclerview;
+
+import java.util.List;
+
+public class RestPokemonResponse {
+    private Integer count;
+    private String next;
+    private List<Pokemon> results;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public List<Pokemon> getResults() {
+        return results;
+    }
+}
